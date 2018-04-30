3dec8440c18ae0e810e4e60d3de1f901c0eadfac
Aniko T <a.nitto309@gmail.com>
2018-04-30 15:34:20 +0000

    Needs Refactoring

--- katas.rb
+++ katas.rb
@@ -1,8 +1,22 @@
+
+
+
 def caffeineBuzz(n)
-  if n%3 == 0
-    return "Java"
-  elsif n%3 == 0 && n%4 == 0
+
+  # if n%3 == 0 && n%4 == 0 
+  #   return "Coffee"
+  # end
+  
+  if n%3 == 0 && n%4 == 0 && n.odd?
     return "Coffee"
+  elsif n%3 == 0 && n%4 == 0 && n.even?
+    return "CoffeeScript"
+  end
+  
+  if n%3 == 0 && n.odd?
+    return "Java"
+  elsif n%3 == 0 && n.even?
+    return "JavaScript"
   end
 end
 
--- katas_1.rb
+++ katas_1.rb
@@ -5,14 +5,17 @@ RSpec.describe do
     describe 'Caffeine Script kata' do
         
         it 'should return java if n can be divided by 3' do
-            
             expect(caffeineBuzz(3)).to eq("Java")
-            
         end
+        
+        it 'should return javascript if n can be divided by 3 and is even' do
+            expect(caffeineBuzz(6)).to eq("JavaScript")
+        end
+        
         
         it 'should return coffee if n can be divided by 3 AND 4' do
             
-            expect(caffeineBuzz(3)).to eq("Java")
+            expect(caffeineBuzz(12)).to eq("CoffeeScript")
             
         end
     end
