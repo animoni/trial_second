7d7b40c5d78f69eeb084b3b09b44297174dc31e8
Aniko T <a.nitto309@gmail.com>
2018-04-30 15:21:44 +0000

    Caffeine Script

--- katas.rb
+++ katas.rb
@@ -1,5 +1,12 @@
-def title_case(title, minor_words)
-  puts
+def caffeineBuzz(n)
+  if n%3 == 0
+    return "Java"
+  elsif n%3 == 0 && n%4 == 0
+    return "Coffee"
+  end
 end
 
-title_case('a clash of KINGS', 'a of')#'A Clash of Kings')
+caffeineBuzz(1)#, "mocha_missing!")
+caffeineBuzz(3)#, "Java")
+caffeineBuzz(6)#, "JavaScript")
+caffeineBuzz(12)#, "CoffeeScript")
--- katas_1.rb
+++ katas_1.rb
@@ -1,4 +1,20 @@
+require_relative "katas.rb"
 
-def current_player
+RSpec.describe do 
+    
+    describe 'Caffeine Script kata' do
+        
+        it 'should return java if n can be divided by 3' do
+            
+            expect(caffeineBuzz(3)).to eq("Java")
+            
+        end
+        
+        it 'should return coffee if n can be divided by 3 AND 4' do
+            
+            expect(caffeineBuzz(3)).to eq("Java")
+            
+        end
+    end
     
 end
