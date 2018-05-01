024cd103698a99693c9d23dc598714825259fa05
Aniko T <a.nitto309@gmail.com>
2018-05-01 11:51:20 +0000

    Basic subclasses - Adam and Eve

--- katas.rb
+++ katas.rb
@@ -1,27 +1,27 @@
 
+require "pry-byebug"
 
-
-def caffeineBuzz(n)
+# def caffeineBuzz(n)
   
-  base_odd = n%3 == 0 && n.odd?
-  base_even = n%3 == 0 && n.even?
+#   base_odd = n%3 == 0 && n.odd?
+#   base_even = n%3 == 0 && n.even?
 
-  if base_odd && n%4 == 0 
-    return "Coffee"
-  elsif base_even && n%4 == 0 
-    return "CoffeeScript"
-  end
+ 
+#   #binding.pry
+#   base_odd && n%4 == 0 ?  "Coffee" : "CoffeeScript"
+#   # if base_odd && n%4 == 0 
+#   #   return "Coffee"
+#   # elsif base_even && n%4 == 0 
+#   #   return "CoffeeScript"
+#   # end
+#   #base_odd ? "Java" : "JavaScript"
   
-  if base_odd
-    return "Java"
-  elsif base_even
-    return "JavaScript"
-  end
+#   if base_odd
+#     return "Java"
+#   elsif base_even
+#     return "JavaScript"
+#   end
   
-  return "mocha_missing!"
-end
+#   return "mocha_missing!"
+# end
 
-caffeineBuzz(1)#, "mocha_missing!")
-caffeineBuzz(3)#, "Java")
-caffeineBuzz(6)#, "JavaScript")
-caffeineBuzz(12)#, "CoffeeScript")
--- katas_1.rb
+++ katas_1.rb
@@ -2,24 +2,24 @@ require_relative "katas.rb"
 
 RSpec.describe do 
     
-    describe 'Caffeine Script kata' do
+    # describe 'Caffeine Script kata' do
         
-        it 'should return java if n can be divided by 3' do
-            expect(caffeineBuzz(3)).to eq("Java")
-        end
+    #     it 'should return java if n can be divided by 3' do
+    #         expect(caffeineBuzz(3)).to eq("Java")
+    #     end
         
-        it 'should return javascript if n can be divided by 3 and is even' do
-            expect(caffeineBuzz(6)).to eq("JavaScript")
-        end
+    #     it 'should return javascript if n can be divided by 3 and is even' do
+    #         expect(caffeineBuzz(6)).to eq("JavaScript")
+    #     end
         
         
-        it 'should return coffee if n can be divided by 3 AND 4' do
-            expect(caffeineBuzz(12)).to eq("CoffeeScript")
-        end
+    #     it 'should return coffee if n can be divided by 3 AND 4' do
+    #         expect(caffeineBuzz(12)).to eq("CoffeeScript")
+    #     end
         
-        it 'should return mocha_missing! if none of the if statements are evaluated' do
-            expect(caffeineBuzz(1)).to eq("mocha_missing!")
-        end 
+    #     it 'should return mocha_missing! if none of the if statements are evaluated' do
+    #         expect(caffeineBuzz(1)).to eq("mocha_missing!")
+    #     end 
         
-    end
+    # end
 end
